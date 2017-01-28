77c50e054d2f8185abf11f43955e31c5a658565f
Daryl Jewkes <github@daryljewkes.com>
2017-01-28 20:15:15 +0000

    building PDF

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var PDFDocument = require('pdfkit');
 var fs = require('fs');
 
-const purple = "#884639";
+const brown = "#884639";
+const white = "#fff";
+const black = "#000";
 const fullWidth = 595.28;
+const dogLogo = {
+   path: 'images/dog.png',
+   width: 70
+};
+
+const fFont = 'fonts/PraxisCom-Light.ttf';
+
 //Create a document 
 doc = new PDFDocument({
+   margin: 0,
    size: 'A4' // A4: [595.28, 841.89]
 })
  
@@ -13,40 +23,91 @@ doc = new PDFDocument({
 doc.pipe(fs.createWriteStream('pdf/output.pdf'));
  
 // Embed a font, set the font size, and render some text 
-doc.font('fonts/PraxisCom-Light.ttf')
-   .fontSize(25)
-   .text('Some text with an embedded font!', 100, 100)
- 
-doc.image('images/dog.png', 0, 15, {width: 150})
-   .text('Proportional to width', 0, 0)
+
+header(doc, brown, 10);
+
+// var t = 25;   
+// doc.fontSize(12).text('THE', 0, t, {
+//   width: fullWidth,
+//   align: 'center'})
+
+// doc.moveTo((fullWidth/2)-80, t+20)
+//    .lineTo((fullWidth/2)+80, t+20)
+//    .lineWidth(1)
+//    .stroke(brown)
+
+// doc.fontSize(18).text('DICKEN ARMS', 0, t+26, {
+//   width: fullWidth,
+//   align: 'center'})
+
+// doc.moveTo((fullWidth/2)-80, t+44)
+//    .lineTo((fullWidth/2)+80, t+44)
+//    .lineWidth(1)
+//    .stroke(brown)
+
+doc.image(dogLogo.path, (fullWidth/2)-(dogLogo.width/2), 83, {width: dogLogo.width})
+
+doc.font(fFont)
+   .fontSize(20)
+   
+//  FOOTER
+footer(doc);
 
 // Add another page 
 doc.addPage()
-   .fontSize(25)
-   .text('Here is some vector graphics...', 100, 100)
- 
-doc.rect(0, 0, fullWidth, 100).fill(purple);
+doc.rect(0, 0, fullWidth, 80).fill(brown);
 
-// Draw a triangle 
-doc.save()
-   .moveTo(200, 200)
-   .lineTo(100, 250)
-   .lineTo(200, 250)
-   .fill(purple)
- 
-// Apply some transforms and render an SVG path with the 'even-odd' fill rule 
-doc.scale(0.6)
-   .translate(470, -380)
-   .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
-   .fill('#884639', 'even-odd')
-   .restore()
- 
-// Add some text with annotations 
-doc.save()
-   .fillColor(purple)
-   .text('Here is a link!', 100, 100)
-   // .underline(100, 100, 160, 27, color: "#0000FF")
-   .link(100, 100, 160, 27, 'http://google.com/')
+doc.fillColor(white)
+
+header(doc, white, 10);
+
+
+
+//  FOOTER
+footer(doc);
  
 // Finalize PDF file 
 doc.end()
+
+function header(doc, color, top) {
+   var t = top;
+   doc.fontSize(12).text('THE', 0, t, {
+     width: fullWidth,
+     align: 'center'})
+
+   doc.moveTo((fullWidth/2)-80, t+20)
+      .lineTo((fullWidth/2)+80, t+20)
+      .lineWidth(0.5)
+      .stroke(color)
+
+   doc.fontSize(18).text('DICKEN ARMS', 0, t+23, {
+     width: fullWidth,
+     align: 'center'})
+
+   doc.moveTo((fullWidth/2)-80, 57)
+      .lineTo((fullWidth/2)+80, 57)
+      .lineWidth(0.5)
+      .stroke(color)
+}
+
+function footer(doc) {
+   var t = 750
+   doc.fillColor(black).fontSize(12)
+
+   doc.text('THE DICKIN ARMS', 0, t, {
+      width: fullWidth,
+      align: 'center'})
+
+   doc.text('LOPPINGTON, SHREWSBURY, SY4 5SR', 0, t+14, {
+      width: fullWidth,
+      align: 'center'})
+
+   doc.text('T: +44 (0)1939 233471', 0, t+28, {
+      width: fullWidth,
+      align: 'center'})
+
+   doc.text('WWW.THEDICKINARMS.COM', 0, t+52, {
+      width: fullWidth,
+      align: 'center'
+   })
+}
